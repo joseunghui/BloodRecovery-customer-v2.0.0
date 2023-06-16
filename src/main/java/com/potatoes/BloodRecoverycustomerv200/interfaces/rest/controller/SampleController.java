d4909bb3974a826db7c6d9a3233c768b08f36c1f
seung <ooo167@naver.com>
2023-06-16 23:30:00 +0900

    add user

--- src/main/java/com/potatoes/BloodRecoverycustomerv200/interfaces/rest/controller/SampleController.java
+++
@@ -1,7 +0,0 @@
-package com.potatoes.BloodRecoverycustomerv200.interfaces.rest.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class SampleController {
-}
