60c17a1e181ccd40bdacefc2e0c3f01f3671ebcb
seung <ooo167@naver.com>
2023-07-02 14:33:34 +0900

    Database create, user create

--- src/main/java/com/potatoes/BloodRecoverycustomerv200/domain/model/aggregates/User.java
+++ src/main/java/com/potatoes/BloodRecoverycustomerv200/domain/model/aggregates/User.java
@@ -1,14 +1,32 @@
 package com.potatoes.BloodRecoverycustomerv200.domain.model.aggregates;
 
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import java.util.Date;
 
 @Entity
 public class User {
-    @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
-    private long id;
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID", nullable = false)
+    private Long id;
 
-    @Column
-    private String name;
+
+    @Column(name = "USER_NAME", nullable = false)
+    private String userName;
+    @Column(name = "USER_NICKNAME", nullable = false)
+    private String userNickname;
+    @Column(name = "USER_PASSWORD", nullable = false)
+    private String userPassword;
+    @Column(name = "PERSONAL_NO", nullable = false)
+    private String personalNo;
+    @Column(name = "GRADE_SN", nullable = false)
+    private String gradeSn;
+    @Column(name = "DATE", nullable = false)
+    private Date date;
 
 }
