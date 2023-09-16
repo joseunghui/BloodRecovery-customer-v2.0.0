39f37f21d04c491694d1537ecc3e8d75f31c43bd
seung <ooo167@naver.com>
2023-09-17 02:30:27 +0900

    회원가입 시 비밀번호 암호화

--- src/main/java/com/potatoes/BloodRecoverycustomerv200/interfaces/rest/mapper/CustomerMapper.java
+++ src/main/java/com/potatoes/BloodRecoverycustomerv200/interfaces/rest/mapper/CustomerMapper.java
@@ -4,13 +4,27 @@ import com.potatoes.BloodRecoverycustomerv200.config.MapstructConfig;
 import com.potatoes.BloodRecoverycustomerv200.domain.model.commands.CustomerCommand;
 import com.potatoes.BloodRecoverycustomerv200.interfaces.rest.dto.CustomerFormDto;
 import org.mapstruct.AfterMapping;
+import org.mapstruct.BeforeMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.transaction.Transactional;
 
 @Mapper(config = MapstructConfig.class)
 public abstract class CustomerMapper {
 
+    // 암호화 Security
+    private BCryptPasswordEncoder encoder;
+
     public abstract CustomerCommand dtoToCommand(CustomerFormDto form);
+
+    // 회원 가입 처리 전에 비밀번호 암호화 상태로 DB 저장
+    @Transactional
+    @BeforeMapping
+    public String getPasswordEncoding(CustomerCommand command) {
+        return encoder.encode(command.getPassword());
+    }
 
     @AfterMapping
     protected void afterMappingToCommand(
