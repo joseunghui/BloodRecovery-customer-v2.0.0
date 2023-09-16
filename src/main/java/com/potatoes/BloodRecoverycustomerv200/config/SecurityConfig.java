0f0cebf1149b228db7eb13f7470207125ec75a84
seung <ooo167@naver.com>
2023-09-17 02:12:54 +0900

    스프링 시큐리티 추가

--- src/main/java/com/potatoes/BloodRecoverycustomerv200/config/SecurityConfig.java
+++ src/main/java/com/potatoes/BloodRecoverycustomerv200/config/SecurityConfig.java
@@ -7,12 +7,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfiguration {
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     @Order(SecurityProperties.BASIC_AUTH_ORDER)
